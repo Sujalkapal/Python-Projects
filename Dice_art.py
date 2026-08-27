d7134d13da0c23d3bddef0cb7d4dfe1f1689dd2f
Sujalkapal <sujalkapal@gmail.com>
2026-08-27 19:46:36 +0530

    Added some comments for better understanding

--- Dice_art.py
+++ Dice_art.py
@@ -63,10 +63,12 @@ num_of_dice = int(input("How many dice? : "))
 for die in range(num_of_dice):
     dice.append(random.randint(1,6))
 
-# for die in range(num_of_dice):
+# for die in range(num_of_dice):               THIS PRINTS THE DICES ONE BELOW THE OTHER 
 #     for line in dice_art.get(dice[die]):
 #         print(line)
 
+
+# THIS PRINTS THE DICE SIDE BY SIDE
 for line in range(7):
     for die in dice:
         print(dice_art.get(die)[line], end = "")
